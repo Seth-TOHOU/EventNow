7a21850f75f2f693ddd0e6764cca4260391666b2
Seth-TOHOU <tohouseth@gmail.com>
2025-12-10 10:08:12 +0100

    Mise en place de la base de donnée

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmIQSovV4w4d11UfSiuY3sQhx8Y0B2130",
+  authDomain: "eventnow-6acb5.firebaseapp.com",
+  projectId: "eventnow-6acb5",
+  storageBucket: "eventnow-6acb5.firebasestorage.app",
+  messagingSenderId: "226415954916",
+  appId: "1:226415954916:web:d4546e66704f206b0815f1",
+  measurementId: "G-6GBG07NMME"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
